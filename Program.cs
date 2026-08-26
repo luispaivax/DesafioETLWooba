e03c2071b4a5f7398005e70b32dcf7c44b415e8b
luispaivax <paivaluis005@gmail.com>
2026-08-26 20:38:15 -0300

    feat: adicionar interface para a etapa de leitura

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
 ﻿using DesafioETLWooba.Models;
 using DesafioETLWooba.Services;
+using DesafioETLWooba.Interfaces;
 
 Console.WriteLine("+=+= ETL =+=+\n");
 
 string caminhoCsv = "clientes_lote_a.csv";
 
-var csvReaderService = new CsvReaderService();
+IReader reader = new CsvReaderService();
 
 try
 {
     // 1. Leitura
     Console.WriteLine("Lendo o CSV...");
-    List<Cliente> registros = csvReaderService.Read(caminhoCsv);
+    List<Cliente> registros = reader.Read(caminhoCsv);
     Console.WriteLine($"{registros.Count} linhas lidas.\n");
 
     // Teste de leitura temporário
